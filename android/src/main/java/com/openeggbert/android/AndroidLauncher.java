5f429e1c2ef068c740d8206059d963f2bd2ccc43
Robert Vokac <mail@robertvokac.com>
2024-07-15 18:58:45 +0200

    Added files

---
+++ android/src/main/java/com/openeggbert/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.openeggbert.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.openeggbert.OpenEggbertMainClass;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new OpenEggbertMainClass(), configuration);
+    }
+}
